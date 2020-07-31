53a334da951afedc7108132bcebc0b1cb03b171d
Louis Gabriel <louis.gabriel@orange.fr>
2020-07-31 23:07:00 +0200

    feature: add a 'cheeses' app containing a Cheese model

---
+++ everycheese/everycheese/cheeses/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from autoslug import AutoSlugField
+from model_utils.models import TimeStampedModel
+
+
+class Cheese(TimeStampedModel):
+
+    class Firmness(models.TextChoices):
+        UNSPECIFIED = "non spécifié", "Non spécifié"
+        SOFT = "pâte molle", "Pâte molle"
+        SEMI_SOFT = "pâte demi-molle", "Pâte demi-molle"
+        SEMI_HARD = "pâte demi-dure", "Pâte demi-dure"
+        HARD = "pâte dure"
+
+    name = models.CharField("nom du fromage", max_length=255)
+    slug = AutoSlugField(
+        "url du fromage",
+        unique=True, always_update=False, populate_from="name",
+    )
+    description = models.TextField("description", blank=True)
+    firmness = models.CharField(
+        "fermeté", max_length=20,
+        choices=Firmness.choices, default=Firmness.UNSPECIFIED,
+    )
